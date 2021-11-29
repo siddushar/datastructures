cc82249282b4c300495d87eda92dde62d7c1ef16
siddushar <siddushar@gmail.com>
2021-11-29 19:59:23 +0530

    queue impl using linked list

---
+++ javascript/queue.js
@@ -0,0 +1,54 @@
+// Using Linked List
+
+class Node {
+  
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Queue {
+ constructor() {
+   this.first = null;
+   this.last = null;
+   this.length = 0;
+ }
+  
+  peek() {
+    return this.first;
+  }
+  
+  enqueue(value) {
+    const newNode = new Node(value);
+    if (this.length == 0) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      this.last.next = newNode;
+      this.last = newNode;
+    }
+    this.length++;
+    return this;
+  }
+  
+  dequeue() {
+    if (this.length == 0) {
+      this.first = null;
+      this.last = null
+    } else {
+      const item = this.first;
+      this.first = this.first.next;
+    }
+    this.length--;
+    return this;
+  }
+  
+}
+
+const myQueue = new Queue()
+myQueue.enqueue("js")
+myQueue.enqueue("ruby")
+myQueue.enqueue("java")
+myQueue.dequeue()
+console.log(myQueue)
