7b29d51a234c71815f7954a43eac598ad4d8cc4f
siddushar <siddushar@gmail.com>
2022-01-07 10:25:37 +0530

    bubble sort implementation using ruby

--- ruby/sorting/bubble_sort.rb
+++ ruby/sorting/bubble_sort.rb
@@ -1,11 +1,35 @@
+array = [34,3,53,3,52,6,9,198,35,21]
+
+# Using for loop
 def bubble_sort(array)
    len = array.length
-   for i in 0..len do
-      for j in 0...len do
-        # Swap numbers
-         temp = array[j]
-         array[j] = array[j+1]
-         array[j+1]= temp
-      end
+   for i in 0...len do
+     for j in 0...len-1 do
+         if array[j] > array[j+1]
+            array[j], array[j+1] = array[j+1], array[j]
+         end
+     end
    end
+   array
 end
+
+# Using while loop
+def bubble_sort(array)
+   array_length = array.size
+   return array if array_length <= 1
+    
+   loop do
+      swapped = false
+      (array_length-1).times do |i|
+         if array[i] > array[i+1]
+            array[i], array[i+1] = array[i+1], array[i]
+            swapped = true
+         end
+      end
+        
+      break if !swapped
+    end
+    array
+end
+
+puts bubble_sort(array)
