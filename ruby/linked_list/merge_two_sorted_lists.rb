89db1cbc6f5d949f6ea4e1b28b5c94a7e550b4a6
siddushar <siddushar@gmail.com>
2022-01-16 21:46:45 +0530

    Merge Two Sorted Lists
    You are given the heads of two sorted linked lists list1 and list2.
    Merge the two lists in a one sorted list. The list should be made by splicing together the nodes of the first two lists.
    Return the head of the merged linked list.

--- ruby/linked_list/merge_two_sorted_lists.rb
+++ ruby/linked_list/merge_two_sorted_lists.rb
@@ -7,12 +7,13 @@
 #     end
 # end
 
+# iterative way with using 3rd list
 def merge_two_lists(list1, list2)
     
     return list1 if list2.nil?
     return list2 if list1.nil?
   
-# iterative way with using 3rd list
+
     mergSorted = ListNode.new
     tail = mergSorted
     
@@ -30,14 +31,36 @@ def merge_two_lists(list1, list2)
     
     tail.next = list1.nil? ? list2 : list1
     return mergSorted.next
-    
-  
-  # recursion in place
+end
+
+# recursion in place
+def merge_two_lists1(list1, list2)
     if list1.val < list2.val
         list1.next = merge_two_lists(list1.next, list2)
-            return list1
-        else
+        return list1
+    else
         list2.next = merge_two_lists(list1, list2.next)
-            return list2
+        return list2
+    end
+end
+
+def merge_two_lists(list1, list2)
+    dummy_node = ListNode.new()
+    tail = dummy_node
+    
+    while(list1 !=nil && list2 != nil)
+        
+        if(list1.val < list2.val)
+            tail.next = list1
+            list1 =  list1.next
+            tail = tail.next
+        else
+            tail.next = list2
+            list2 =  list2.next
+            tail = tail.next
         end
+        
+    end
+    tail.next = (list1 != nil) ? list1 : list2
+    dummy_node.next
 end
